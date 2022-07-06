45cf360c55156bf47c6c7270a764647b6efaf7ec
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2022-07-06 12:59:06 +0900

    Update dijkstra_heapq.py

--- Algorithm/Dijkstra/dijkstra_heapq.py
+++ Algorithm/Dijkstra/dijkstra_heapq.py
@@ -7,6 +7,7 @@ n, m = map(int, input().split())
 start = int(input())
 
 graph = [[]for i in range(n+1)]
+#최단거리 테이블
 distance = [INF] * (n + 1)
 
 #간선 값 입력
@@ -21,10 +22,18 @@ def dijkstra(start):
     distance[start] = 0
     while q:
         dist, now = heapq.heappop(q)
+        print("*" * 30)
+        print("현재 노드 : ", now)
+        print("현재 최소거리 : ", dist)
         if distance[now] < dist:
+            print("이미 방문한 노드입니다")
             continue
         for i in graph[now]:
+            print("현재 노드 주변 노드 : ", i)
+            print("현재 노드 주변 노드 거리: ", i[1])
             cost = dist + i[1]
+            print("주변노드 거리 + 현재최소거리 : ", cost)
+            #cost 가 최소거리 테이블 값보다 작을경우 푸시
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
@@ -32,8 +41,9 @@ def dijkstra(start):
 
 dijkstra(start)
 
+
 for i in range(1, n+1):
     if distance[i] == INF:
-        print("INFINITY")
+        print("INF")
     else:
         print(distance[i])
