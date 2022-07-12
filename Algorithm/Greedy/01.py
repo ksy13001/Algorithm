4b035b2ee2491f468687c4421296a9317f986892
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2022-07-12 10:15:51 +0900

    Create 01.py

---
+++ Algorithm/Greedy/01.py
@@ -0,0 +1,14 @@
+n = int(input())
+mo = sorted(list(map(int, input().split())))
+group = 0
+while mo:
+    m = mo.pop()
+    if len(set(mo)) == 1:
+        group += 1
+        break
+    for i in range(1, m):
+        if i in mo:
+            mo.remove(i)
+    group += 1
+
+print(group)
