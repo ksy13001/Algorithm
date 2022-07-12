e01c94a7a070a47c31228f6d8a607ab765e3a427
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2022-07-12 11:49:14 +0900

    Create 03.py

---
+++ Algorithm/Greedy/03.py
@@ -0,0 +1,11 @@
+s = input()
+cnt1 = 0
+cnt0 = 0
+for i in range(len(s)):
+    cur = s[i]
+    if s[i-1] != cur:
+        if s[i - 1] == '1':
+            cnt1 += 1
+        else:
+            cnt0 += 1
+print(min(cnt0, cnt1))
