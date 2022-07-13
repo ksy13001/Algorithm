76af2a6c3acc863fd019b44084d2b9f9c1bba758
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2022-07-13 15:37:55 +0900

    라면 사기

--- Algorithm/Greedy/18185.py
+++ Algorithm/Greedy/18185.py
@@ -1,19 +1,23 @@
 n = int(input())
-m = list(map(int, input().split())) # 3, 5, 7
+m = list(map(int, input().split()))+[0, 0] # 3, 5, 7
 result = 0
-for _ in range(2): m.append(0)
 for i in range(n):
-    while m[i] > 0:
-        if m[i + 1] > 0:
-            if m[i + 2] > 0:
-                result += 7
-                for j in range(3):
-                    m[i+j] -= 1
-            else:
-                result += 5
-                for j in range(2):
-                    m[i+j] -= 1
-        else:
-            result += 3
-            m[i] -= 1
+    target = min(m[i:i + 3])
+    if target > 0:
+        if m[i+1] > m[i+2]:
+            target = min(m[i+1] - m[i+2], m[i])
+            for j in range(2): m[i + j] -= target
+            result += 5 * target
+        if m[i] > 0:
+            target = min(m[i:i + 3])
+            for j in range(3): m[i + j] -= target
+            result += 7 * target
+    else:
+        target = min(m[i:i+2])
+        if target > 0:
+            for j in range(2): m[i + j] -= target
+            result += 5 * target
+    if m[i] > 0:
+        result += 3 * m[i]
+        m[i] = 0
 print(result)
