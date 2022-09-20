5808788fbe54b8f5920a8dd03098a39eda4b81ff
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2022-09-20 11:16:56 +0900

    문자열 뒤집기

--- Algorithm/Greedy/03.py
+++ Algorithm/Greedy/03.py
@@ -9,3 +9,20 @@ for i in range(len(s)):
         else:
             cnt0 += 1
 print(min(cnt0, cnt1))
+"""
+s = input()
+now = s[-1]
+one = 0
+zero = 0
+for st in s:
+    if st == '0':
+        if now == '1':
+            now = '0'
+            zero += 1
+    if st == '1':
+        if now == '0':
+            now = '1'
+            one += 1
+
+print(min(one, zero))
+"""
