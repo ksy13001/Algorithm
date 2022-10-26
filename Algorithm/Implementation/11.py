611cda2d7aa3c24bb01c4bc426c5cc8ba82cab8d
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2022-10-26 11:54:33 +0900

    뱀

--- Algorithm/Implementation/11.py
+++ Algorithm/Implementation/11.py
@@ -1,56 +1,44 @@
 from collections import deque
 n = int(input())
 k = int(input())
-stage = [[0 for _ in range(n)]for _ in range(n)]
-dir = []
-cur_dir = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-time = 0
-result = 0
-i = 0
-snake_size = 0
-route = []
-snake = deque()
-snake.append((0, 0))
+graph = [[0 for _ in range(n)] for _ in range(n)]
 for _ in range(k):
     a, b = map(int, input().split())
-    stage[b-1][a-1] = 1
-#뱀의 방향 변환 정보
+    graph[a-1][b-1] = 1
 l = int(input())
+move = []
 for _ in range(l):
-    a, b = map(str, input().split())
-    dir.append((int(a), b))
-cnt = 0
+    c, d = map(str, input().split())
+    move.append((int(c), d))
+dn = 0
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+time = 0
+tm = 0
+snake = deque()
+now = [0, 0]
+snake.append((now[0], now[1]))
 while True:
-    print('*'*40)
-    cur = snake.pop()
-    route.append(cur)
-    print("현재위치 : ", cur)
-    print('time: ', time)
-    print('snake size : ', len(snake)+1)
-    print("route : ", route)
-    if cur[0] >= n or cur[1] >= n or cur[0] < 0 or cur[1] < 0:
-        print("범위를 벗어남")
-        break
-    if cur in snake:
-        print("겹침")
-        break
-    if stage[cur[0]][cur[1]] == 1:
-        print("사과발견 지점 : ", cur)
-        snake_size += 1
-        stage[cur[0]][cur[1]] = 0
-    if time == dir[cnt][0]:
-        if dir[cnt][1] == 'D':
-            print("우회전***************")
-            i += 1
-        else:
-            print("좌회전***************")
-            i -= 1
-        cnt += 1
-        if cnt > 3:
-            cnt = 0
-    for k in range(snake_size):
-        snake.append(route[len(route)-1-k])
-    snake.append((cur[0] + cur_dir[i][0], cur[1] + cur_dir[i][1]))
-    print('snake : ',*snake)
+    if tm < l:
+        if time == move[tm][0]:
+            if move[tm][1] == 'D':
+                dn += 1
+            else:
+                dn -= 1
+            dn %= 4
+            tm += 1
+    graph[now[0]][now[1]] = 2
+    now[0] += dx[dn]
+    now[1] += dy[dn]
     time += 1
- 
+    if now[0] < 0 or now[0] >= n or now[1] < 0 or now[1] >= n or graph[now[0]][now[1]] == 2:
+        print(time)
+        print(snake)
+        break
+    snake.append((now[0], now[1]))
+    print(now, end=' ')
+    if graph[now[0]][now[1]] == 0:
+        x, y = snake.popleft()
+        graph[x][y] = 0
+    graph[now[0]][now[1]] = 2
+    print(snake)
