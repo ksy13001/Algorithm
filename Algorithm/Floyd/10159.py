e96c28a24122a38c7a3bd52d9b3d7bb530d9538c
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2022-12-06 15:41:12 +0900

    저울

---
+++ Algorithm/Floyd/10159.py
@@ -0,0 +1,22 @@
+INF = int(1e9)
+n = int(input())
+m = int(input())
+graph = [[INF]*(n+1) for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = -1
+
+for i in range(1, n+1):
+    graph[i][i] = 0
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if graph[i][j] == INF:
+                if graph[i][k] == 1 and graph[k][j] == 1:
+                    graph[i][j] = 1
+                elif graph[i][k] == -1 and graph[k][j] == -1:
+                    graph[i][j] = -1
+for i in range(1, n + 1):
+    print(graph[i].count(INF)-1)
