d2b766efffa128085dc345a732d416a7f201f892
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2023-01-25 15:57:56 +0900

    도로포장 (이중리스트를 사용한 다익스트라)

---
+++ Algorithm/Dijkstra/1162.py
@@ -0,0 +1,34 @@
+import heapq
+import sys
+input = sys.stdin.readline
+INF = sys.maxsize
+n, p, k = map(int, input().split())
+graph = [[]for _ in range(n+1)]
+distance = [[INF for _ in range(k+1)]for _ in range(n+1)]
+for _ in range(p):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+
+def dijkstra():
+    q = []
+    heapq.heappush(q, (0, 1, 0))
+    distance[1][0] = 0
+    while q:
+        dist, now, cnt = heapq.heappop(q)
+        if distance[now][cnt] < dist:
+            continue
+        for n_node, n_dist in graph[now]:
+            cost = n_dist + dist
+            if cost < distance[n_node][cnt]:
+                distance[n_node][cnt] = cost
+                heapq.heappush(q, (cost, n_node, cnt))
+            if cnt < k and dist < distance[n_node][cnt+1]:
+                distance[n_node][cnt+1] = dist
+                heapq.heappush(q, (dist, n_node, cnt+1))
+
+
+dijkstra()
+print(min(distance[n]))
+
