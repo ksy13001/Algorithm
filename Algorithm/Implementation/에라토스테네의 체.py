93180ff1fc86d49901d5d4bab6bd5056e46c8d46
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2023-03-06 13:27:10 +0900

    소수 판별 알고리즘 ( 에라토스테네의 체)

---
+++ Algorithm/Implementation/에라토스테네의 체.py
@@ -0,0 +1,25 @@
+import math
+"""
+1. 2~n 까지 모든 자연수 나열
+2. 나열된 수 중 가장 작은수 x 지정
+3. 나열 된 수 중 x 배수 제거
+4. 2,3 반복
+
+"""
+
+n = int(input())
+arr = [True] * (n + 1) #n 까지 자연수 소수로 두기
+
+for i in range(2, int(math.sqrt(n))+1): # 2 ~ n 까지
+  if arr[i]: # i가 소수면
+    j = 2
+    while i * j <= n:
+      # i 제외 n 이하 i의 배수 모두 삭제
+      arr[i * j] = False
+      j += 1
+
+ans = []
+for i in range(2, n+1):
+  if arr[i]:
+    ans.append(i)
+  
