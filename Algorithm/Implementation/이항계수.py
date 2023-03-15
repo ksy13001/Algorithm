5fa0cc8b87588b9de0105b893c7d30711fa56359
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2023-03-15 13:38:18 +0900

    이항계수 - DP

---
+++ Algorithm/Implementation/이항계수.py
@@ -0,0 +1,13 @@
+def com(n, k):
+    dp = [[0 for _ in range(n+1)]for _ in range(n+1)]
+    for i in range(n+1):
+        dp[i][0] = 1
+        dp[i][i] = 1
+    for i in range(1, n+1):
+        for j in range(1, k+1):
+            dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+    return dp[n][k]
+
+
+n, m = map(int, input().split())
+print(com(n, m))
