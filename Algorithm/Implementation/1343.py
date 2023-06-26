08923ad740ec406d31e67608cc114e2681c1e2e0
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2023-06-27 00:18:47 +0900

    폴리오미노 (그리디 or 문자열.replace())

--- Algorithm/Implementation/1343.py
+++ Algorithm/Implementation/1343.py
@@ -1,3 +1,14 @@
+
+#replace 이용하여 아주 쉽게 풀이 가능
+s = input()
+s1 = s.replace('XXXX', 'AAAA')
+s2 = s1.replace('XX', 'BB')
+if 'X' in s2:
+    print(-1)
+else:
+    print(s2)
+
+
 def sol():
     s = input().split('.')
     arr = []
@@ -16,4 +27,3 @@ def sol():
                 print(-1)
                 return 0
     print(''.join(arr[:-1]))
-sol()
