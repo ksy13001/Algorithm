6efccbba27b18bde288e399cbc8541a1d0e44760
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2023-08-01 16:17:10 +0900

    Update 비트마스킹.py

--- Tip/비트마스킹.py
+++ Tip/비트마스킹.py
@@ -26,3 +26,27 @@ print([i for i in range(1<<5)]) # [0, 1, 2 ... 31]
 
 print(bin(0b1111 & 1<<3)) # 0b1111 AND 0b1000 -> 0b1000
 print(bin(0b1111 ^ 1<<3)) # 0b1111 XOR 0b1000 -> 0b111
+
+# i 번째 비트 활성화 a|(1<<i)
+a = 0b0000
+for i in range(4):
+    print(bin(a|(1<<i)))
+"""
+0b0001
+0b0010
+0b0100
+0b1000
+"""
+
+# i 번째 비트 해제하기 a&!(1<<i)
+a = 1111
+for i in range(4):
+    print(bin(a&(~(1<<i))))
+
+# i 번째 비트 활성화 됐는지 확인 a&&(1<<i)
+a = 0b0010
+for i in range(4):
+    if a&(1<<i):
+        print(i, bin(a&(1<<i)))
+
+
