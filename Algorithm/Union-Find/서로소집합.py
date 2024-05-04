f9380e28bfc4377bd4f9eba0ded8bfc1355f36b1
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2024-05-04 22:45:09 +0900

    Update 서로소집합.py

--- Algorithm/Union-Find/서로소집합.py
+++ Algorithm/Union-Find/서로소집합.py
@@ -1,1 +1,15 @@
 
+def find(parents, x): # x의 부모 찾기
+    if parents[x] != x:
+        return find(parents, parents[x]) # x
+    else:
+        return x
+
+
+def union(parents, a, b):   # a, b 합치기
+    a = find(parents, a)
+    b = find(parents, b)
+    if a < b:
+        parents[b] = a
+    else:
+        parents[a] = b
