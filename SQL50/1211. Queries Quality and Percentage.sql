f8494fc31c9c02c712ecb4a297d4814793a48beb
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2025-04-11 21:01:47 +0900

    통계함수에 조건문 걸기

--- SQL50/1211. Queries Quality and Percentage.sql
+++ SQL50/1211. Queries Quality and Percentage.sql
@@ -1,16 +1,24 @@
-# Write your MySQL query statement below
-WITH Poor_query AS (
-    SELECT query_name AS query_name, COUNT(*) AS poor_query_cnt
-    FROM Queries
-    WHERE rating < 3
-    GROUP BY query_name
-)
+SELECT
+    query_name,
+    ROUND(AVG(rating/position),2) AS quality,
+    ROUND(AVG(rating<3)*100,2) AS poor_query_percentage
+FROM Queries
+GROUP By query_name
 
-SELECT 
-    q1.query_name, 
-    ROUND(AVG(q1.rating/q1.position),2) AS quality,
-    IFNULL(ROUND(pq.poor_query_cnt / COUNT(*) * 100,2),0) AS poor_query_percentage
-FROM Queries q1
-LEFT JOIN Poor_query pq
-    ON pq.query_name = q1.query_name
-GROUP BY q1.query_name
+
+-- # Write your MySQL query statement below
+-- WITH Poor_query AS (
+--     SELECT query_name AS query_name, COUNT(*) AS poor_query_cnt
+--     FROM Queries
+--     WHERE rating < 3
+--     GROUP BY query_name
+-- )
+
+-- SELECT 
+--     q1.query_name, 
+--     ROUND(AVG(q1.rating/q1.position),2) AS quality,
+--     IFNULL(ROUND(pq.poor_query_cnt / COUNT(*) * 100,2),0) AS poor_query_percentage
+-- FROM Queries q1
+-- LEFT JOIN Poor_query pq
+--     ON pq.query_name = q1.query_name
+-- GROUP BY q1.query_name
