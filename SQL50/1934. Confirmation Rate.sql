cb9af8a136caa1794d3e421a30f23d11c5710571
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2025-05-26 15:56:25 +0900

    case when then

--- SQL50/1934. Confirmation Rate.sql
+++ SQL50/1934. Confirmation Rate.sql
@@ -1,8 +1,12 @@
-SELECT 
-    s.user_id,
-    ROUND(SUM(CASE WHEN c.action="confirmed" THEN 1 ELSE 0 END) / COUNT(*) ,2) AS confirmation_rate
-FROM
-    Signups s
+SELECT
+    s.user_id, 
+    ROUND(
+        SUM(CASE 
+                WHEN c.action = 'confirmed' THEN 1
+                ELSE  0 
+            END) / COUNT(*)
+         ,2) AS confirmation_rate
+FROM Signups s
 LEFT JOIN Confirmations c
-ON s.user_id = c.user_id
+    ON s.user_id = c.user_id
 GROUP BY s.user_id
