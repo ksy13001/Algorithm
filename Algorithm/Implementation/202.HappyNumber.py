b6be51d940d4feb45e075c9e48731a4c2a1b3b65
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2025-06-11 23:18:44 +0900

    Update 202.HappyNumber.py

--- Algorithm/Implementation/202.HappyNumber.py
+++ Algorithm/Implementation/202.HappyNumber.py
@@ -1,16 +1,14 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        visited = []
-        while n != 1:
+        visited = set()
+        while n != 1 and n not in visited:
+            visited.add(n)
             n = self.digit_sum(n)
-            if n in visited:
-                return False
-            visited.append(n)
-        return True
+        return n==1
 
     def digit_sum(self, num:int)->int:
         result = 0
-        while num >= 10:
+        while num > 0:
             result += (num%10)**2
             num //= 10
-        return result + num**2
+        return result
