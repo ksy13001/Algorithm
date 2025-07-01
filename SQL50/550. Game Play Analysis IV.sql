6e4ecc5c246e88d914a65532c212c2e310713c7e
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2025-07-01 22:09:46 +0900

    서브쿼리, 셀프조인, DATEDIFF

--- SQL50/550. Game Play Analysis IV.sql
+++ SQL50/550. Game Play Analysis IV.sql
@@ -1,6 +1,5 @@
 SELECT 
-    ROUND(COUNT(DISTINCT a1.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity) 
-    ,2) AS fraction
+    ROUND(COUNT(DISTINCT a1.player_id) / COUNT(*) ,2) AS fraction
 FROM Activity a1
-JOIN (SELECT player_id, min(event_date) AS first_date FROM Activity GROUP BY player_id) a2
+RIGHT JOIN (SELECT player_id, min(event_date) AS first_date FROM Activity GROUP BY player_id) a2
     ON a1.player_id = a2.player_id AND DATEDIFF(a1.event_date, a2.first_date) = 1
