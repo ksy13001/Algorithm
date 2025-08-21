8f0cec446e7f95e18d6ba46a4723807bbeda3721
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2025-08-21 15:46:10 +0900

    Update 70. Climbing Stairs.py

--- Algorithm/Dynamic_Programming/70. Climbing Stairs.py
+++ Algorithm/Dynamic_Programming/70. Climbing Stairs.py
@@ -1,6 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [0, 1, 2] + [0] * 45
+        dp_1 = 1
+        dp_2 = 2
+        if(n<=2):
+            return n
         for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
+            dp_n = dp_1 + dp_2
+            dp_1 = dp_2
+            dp_2 = dp_n
+        return dp_n
