2a70e678418e902f46c5256a9d8abb8ea6d5f52c
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2025-08-22 18:01:01 +0900

    String + dp

--- Algorithm/Dynamic_Programming/139. Word Break.py
+++ Algorithm/Dynamic_Programming/139. Word Break.py
@@ -1,22 +1,5 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        n = len(s)
-        max_length = max(map(lambda x:len(x), wordDict))
-        min_length = min(map(lambda x:len(x), wordDict))
-        dp = [False for _ in range(n+1)]
-        dp[0] = True
-        for i in range(1, n+1):
-            for length in range(min_length, max_length+1):
-                j = i - length
-                if j<0:
-                    continue
-                if dp[j] and s[j:i] in wordDict:
-                    dp[i] = True
-                    break
-        return dp[n]
-
-class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         if len(s) < min(map(len, wordDict)):
             return False
         n = len(s)
@@ -24,6 +7,9 @@ class Solution:
         dp[0] = True
         for i in range(1, n+1):
             for w in wordDict:
+                j = i-len(w)
+                if j<0:continue
                 if dp[i-len(w)] and s[i-len(w):i] == w:
                     dp[i] = True
+                    break
         return dp[n]
