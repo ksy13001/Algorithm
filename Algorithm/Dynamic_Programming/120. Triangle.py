c5cc2f7649f13c03661d9057a251e688461e367c
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2025-09-02 22:16:12 +0900

    bottom-up

--- Algorithm/Dynamic_Programming/120. Triangle.py
+++ Algorithm/Dynamic_Programming/120. Triangle.py
@@ -1,8 +1,7 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        n = len(triangle)
-        dp = triangle[-1]
-        for i in range(n-2, -1, -1):
-            for idx, value in enumerate(triangle[i]):
-                dp[idx] = min(dp[idx], dp[idx+1]) + value
+        dp = triangle[-1][:]
+        for i in range(len(triangle)-2, -1, -1):
+            for idx, val in enumerate(triangle[i]):
+                dp[idx] = min(dp[idx], dp[idx+1]) + val
         return dp[0]
