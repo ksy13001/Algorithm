6172158da7a484ae6215ff2d0bd8d920b305665c
ksy13001 <57353099+ksy13001@users.noreply.github.com>
2025-09-03 19:41:19 +0900

    dp

---
+++ Algorithm/Dynamic_Programming/64. Minimum Path Sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        for i in range(1, m):
+            grid[0][i] += grid[0][i-1]
+        for i in range(1, n):
+            for j in range(m):
+                if j == 0:
+                    grid[i][j] += grid[i-1][j]
+                else:
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        return grid[n-1][m-1]
